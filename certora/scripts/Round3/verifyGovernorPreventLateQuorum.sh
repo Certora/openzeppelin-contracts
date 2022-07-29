742adc99930cb5011f4cfbc9fe5406576f80e0b9
Michael George <mdgeorge@cs.cornell.edu>
2022-07-29 18:40:49 -0400

    bug fix in script

--- certora/scripts/Round3/verifyGovernorPreventLateQuorum.sh
+++ certora/scripts/Round3/verifyGovernorPreventLateQuorum.sh
@@ -1,5 +1,5 @@
 certoraRun \
-    certora/harnesses/ERC721VotesHarness.sol certora/munged/governance/TimelockController.sol certora/harnesses/GovernorPreventLateQuorumHarness.sol \
+    certora/harnesses/ERC721VotesHarness.sol certora/munged/governance/TimelockController.sol certora/harnesses/GovernorPreventLateQuorumHarness.sol certora/harnesses/ERC20VotesHarness.sol \
     --verify GovernorPreventLateQuorumHarness:certora/specs/GovernorPreventLateQuorum.spec \
     --solc solc8.4 \
     --optimistic_loop \
